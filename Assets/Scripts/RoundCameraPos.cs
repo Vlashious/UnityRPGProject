64d75b2a537e9598f41025f4992c3323e25be1b5
Vlashious <flashro561@gmail.com>
2020-10-27 22:21:26 +0300

    stabilizer for cinemachine vcam + material for tilemap

---
+++ Assets/Scripts/RoundCameraPos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class RoundCameraPos : CinemachineExtension
+{
+    public float PixelsPerUnit = 32;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float delta)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            Vector3 pos = state.FinalPosition;
+            Vector3 pos2 = new Vector3(Round(pos.x), Round(pos.y), pos.z);
+
+            state.PositionCorrection += pos2 - pos;
+        }
+    }
+
+    private float Round(float x)
+    {
+        return Mathf.Round(x * PixelsPerUnit) / PixelsPerUnit;
+    }
+}
