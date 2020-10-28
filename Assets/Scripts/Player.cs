7e1b3ad4148693db1c6de62da41ef1920486a248
Vlashious <flashro561@gmail.com>
2020-10-28 17:47:05 +0300

    added coin to the scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Pickable"))
+        {
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
