4bef4842c01d39fa530c31d368945a0b12eac9c9
Vlashious <flashro561@gmail.com>
2020-10-30 10:36:39 +0300

    game manager with spawn points added

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public SpawnPoint playerSpawnPoint;
+    void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+        SetupScene();
+    }
+
+    private void SetupScene()
+    {
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        if (playerSpawnPoint)
+        {
+            GameObject player = playerSpawnPoint.SpawnObject();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefabToSpawn;
+    [SerializeField]
+    private float _repeatInterval;
+    void Start()
+    {
+        if (_repeatInterval > 0)
+        {
+            InvokeRepeating("SpawnObject", 0, _repeatInterval);
+        }
+    }
+    public GameObject SpawnObject()
+    {
+        if (_prefabToSpawn)
+        {
+            return Instantiate(_prefabToSpawn, transform.position, Quaternion.identity);
+        }
+        return null;
+    }
+}
