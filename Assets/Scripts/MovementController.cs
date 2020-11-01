98b617b62b9e1a2fe06d1db9058f0ef7bd619bb4
Vlashious <flashro561@gmail.com>
2020-11-01 20:22:51 +0300

    use blend trees for player animation

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,18 +7,9 @@ public class MovementController : MonoBehaviour
 {
     [SerializeField]
     private float _movementSpeed = 3.0f;
-    private enum CharStates
-    {
-        walkEast = 1,
-        walkSouth = 2,
-        walkWest = 3,
-        walkNorth = 4,
-        idle = 5
-    }
     private Vector2 _movement = new Vector2();
     private Rigidbody2D _rBody;
     private Animator _animator;
-    private string animState = "AnimationState";
     void Start()
     {
         _rBody = GetComponent<Rigidbody2D>();
@@ -31,11 +22,17 @@ public class MovementController : MonoBehaviour
 
     private void UpdateState()
     {
-        if (_movement.x > 0) _animator.SetInteger(animState, (int)CharStates.walkEast);
-        else if (_movement.x < 0) _animator.SetInteger(animState, (int)CharStates.walkWest);
-        else if (_movement.y > 0) _animator.SetInteger(animState, (int)CharStates.walkNorth);
-        else if (_movement.y < 0) _animator.SetInteger(animState, (int)CharStates.walkSouth);
-        else _animator.SetInteger(animState, (int)CharStates.idle);
+        if (Mathf.Approximately(_movement.x, 0) && Mathf.Approximately(_movement.y, 0))
+        {
+            _animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", true);
+        }
+
+        _animator.SetFloat("xDir", _movement.x);
+        _animator.SetFloat("yDir", _movement.y);
     }
 
     void FixedUpdate()
